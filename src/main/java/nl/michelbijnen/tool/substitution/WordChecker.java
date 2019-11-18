ef07f6c9d818f1c961248c4e78c114fc35fb4d3c
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2019-11-18 19:24:42 +0100

    Changed way of importing file

--- src/main/java/nl/michelbijnen/tool/substitution/WordChecker.java
+++ src/main/java/nl/michelbijnen/tool/substitution/WordChecker.java
@@ -1,33 +1,33 @@
 package nl.michelbijnen.tool.substitution;
 
 import nl.michelbijnen.tool.substitution.model.ConsoleColors;
+import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordChecker {
     private Set<String> wordsSet;
 
     public WordChecker(int minWordLength) {
         try {
-            Path path = Paths.get("words.txt");
-            byte[] readBytes = Files.readAllBytes(path);
-            String wordListContents = new String(readBytes, "UTF-8");
-            String[] words = wordListContents.split("\n");
-            wordsSet = new HashSet<>();
-            Collections.addAll(wordsSet, words);
+            try (InputStream resource = getClass().getResourceAsStream("/words.txt")) {
+                this.wordsSet = new HashSet<>(new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8)).lines().collect(Collectors.toList()));
+            }
 
             for (String word : new HashSet<>(this.wordsSet)) {
                 if (word.length() < minWordLength) {
                     this.wordsSet.remove(word);
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.print(ConsoleColors.RED);
             System.out.println(e.getMessage());
             for (int i = 0; i < 2147483647; i++) {
